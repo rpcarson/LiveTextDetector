94f48efb29ed57de07828b3a3fa33278074bfcab
mrpc <reed.carson@carvana.com>
2021-05-24 14:12:46 -0400

    Initial Commit

---
+++ LiveTextDetector/LiveTextDetector.h
@@ -0,0 +1,18 @@
+//
+//  LiveTextDetector.h
+//  LiveTextDetector
+//
+//  Created by Reed Carson on 5/24/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LiveTextDetector.
+FOUNDATION_EXPORT double LiveTextDetectorVersionNumber;
+
+//! Project version string for LiveTextDetector.
+FOUNDATION_EXPORT const unsigned char LiveTextDetectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveTextDetector/PublicHeader.h>
+
+
